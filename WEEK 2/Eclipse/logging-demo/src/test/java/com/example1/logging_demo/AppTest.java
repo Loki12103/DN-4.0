8a1fe25054c8cdca78d5b469eee95a5d626fcb30
Lokesh K <lokeshsh5252@gmail.com>
2025-06-30 00:19:39 +0530

    Add files via upload

---
+++ WEEK 2/Eclipse/logging-demo/src/test/java/com/example1/logging_demo/AppTest.java
@@ -0,0 +1,12 @@
+package com.example1.logging_demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
---
+++ WEEK 2/Eclipse/mockito-demo/src/main/java/com/example/mockito_demo/ExternalApi.java
@@ -0,0 +1,4 @@
+package com.example.mockito_demo;
+public interface ExternalApi {
+    String getData();
+}
